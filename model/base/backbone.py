34f42a4ab678329019744ae426d3124308631ba5
CuiJiacheng <cuijiacheng@mail.nwpu.edu.cn>
2025-06-03 14:35:37 +0800

    Update backbone.py

--- model/base/backbone.py
+++ model/base/backbone.py
@@ -1,56 +1,59 @@
 import torch.nn as nn
-from model.base.components import Conv, C2f
+from .components import Conv, C2f, SPPF
+
 
 class Backbone(nn.Module):
+    """
+    Reference: resources/yolov8.jpg
+    """
     def __init__(self, w, r, n):
+
         super().__init__()
         self.imageChannel = 3
         self.kernelSize = 3
         self.stride = 2
 
-        # 定义通道数
-        c1 = int(128 * w)  # 128
-        c2 = int(256 * w)  # 256
-        c3 = int(512 * w)  # 512
-        c4 = int(512 * w * r)  # 1024/768
+        self.l0 = Conv(3, int(64 * w), 3, 2, 1)
+        self.l1 = Conv(int(64 * w), int(128 * w), 3, 2, 1)
+        self.l3 = Conv(int(128 * w), int(256 * w), 3, 2, 1)
+        self.l5 = Conv(int(256 * w), int(512 * w), 3, 2, 1)
+        self.l7 = Conv(int(512 * w), int(512 * w * r), 3, 2, 1)
 
-        # Stem
-        self.stem = Conv(3, c1, 3, 2)  # (B, c1, 320, 320)
+        self.l2 = C2f(int(128 * w), int(128 * w), n, True)
+        self.l4 = C2f(int(256 * w), int(256 * w), 2 * n, True)
+        self.l6 = C2f(int(512 * w), int(512 * w), 2 * n, True)
+        self.l8 = C2f(int(512 * w * r), int(512 * w * r), n, True)
 
-        # Stage 1 (P2/4) 
-        self.stage1_conv = Conv(c1, c2, 3, 2)
-        self.stage1_c2f = C2f(c2, c2, n)
-
-        # Stage 2 (P3/8)
-        self.stage2_conv = Conv(c2, c3, 3, 2) 
-        self.stage2_c2f = C2f(c3, c3, n)
-
-        # Stage 3 (P4/16)
-        self.stage3_conv = Conv(c3, c3, 3, 2)
-        self.stage3_c2f = C2f(c3, c3, n)
-
-        # Stage 4 (P5/32)
-        self.stage4_conv = Conv(c3, c4, 3, 2)
-        self.stage4_c2f = C2f(c4, c4, n)
+        self.l9 = SPPF(int(512 * w * r), int(512 * w * r))
 
     def forward(self, x):
-        # Stem
-        x = self.stem(x)  # (B, c1, 320, 320)
+        """
+        Input shape: (B, 3, 640, 640)
+        Output shape:
+            feat0: (B, 128 * w, 160, 160)
+            feat1: (B, 256 * w, 80, 80)
+            feat2: (B, 512 * w, 40, 40)
+            feat3: (B, 512 * w * r, 20, 20)
+        """
 
-        # Stage 1 (P2/4)
-        x = self.stage1_conv(x)  # (B, c2, 160, 160) 
-        feat0 = self.stage1_c2f(x)  # (B, c2, 160, 160)
+        # Stem Layer
+        stemLayer = self.l0(x)
+        
+        # Stage Layer 1
+        feat0 = self.l1(stemLayer)
+        feat0 = self.l2(feat0)
 
-        # Stage 2 (P3/8)
-        x = self.stage2_conv(feat0)  # (B, c3, 80, 80)
-        feat1 = self.stage2_c2f(x)  # (B, c3, 80, 80)
+        # Stage Layer 2
+        feat1 = self.l3(feat0)
+        feat1 = self.l4(feat1)
 
-        # Stage 3 (P4/16)
-        x = self.stage3_conv(feat1)  # (B, c3, 40, 40)
-        feat2 = self.stage3_c2f(x)  # (B, c3, 40, 40)
+        # Stage Layer 3
+        feat2 = self.l5(feat1)
+        feat2 = self.l6(feat2)
 
-        # Stage 4 (P5/32)
-        x = self.stage4_conv(feat2)  # (B, c4, 20, 20)
-        feat3 = self.stage4_c2f(x)  # (B, c4, 20, 20)
+        # Stage Layer 4
+        feat3 = self.l7(feat2)
+        feat3 = self.l8(feat3)
+        feat3 = self.l9(feat3)
 
         return feat0, feat1, feat2, feat3
